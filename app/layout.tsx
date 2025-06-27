bd8c47a2419d50450bfd1a4dadbd686bd44ef464
Pranay Sanghvi <sanghvipranay17@gmail.com>
2025-06-27 21:52:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Pranay',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/website/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   )
