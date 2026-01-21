ec847542217d6a6717464b9aca40d0392c20952e
Pranay Sanghvi <sanghvipranay17@gmail.com>
2026-01-21 10:40:06 +0530

    Add tech-themed SVG favicon
    - Add gradient P logo favicon for personal branding
    - Update favicon path to use SVG format

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/website/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </head>
       <body>
         <Navigation />
